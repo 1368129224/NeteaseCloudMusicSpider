25bec1fe148840c164f7a2da9435787251967e84
zooter_z <zooter_z@126.com>
2019-04-14 10:53:07 +0800

    更新文档

--- temp.py
+++ temp.py
@@ -1,43 +1,19 @@
-import random
-import subprocess
-import time
-import signal
+import requests
+from fake_useragent import UserAgent
 
 
-class api():
-    def __init__(self):
-        self.port = random.randint(3001, 4000)
-
-    def __changeIP(self):
-        try:
-            with open(r"G:\163music\util\request.js", "r+", encoding="utf8") as f:
-                f.seek(2782)
-                random.seed(a=None)
-                ip = repr(random.randint(100, 255)) + '.' + \
-                     repr(random.randint(100, 255))
-                f.write(ip)
-            return ip
-        except Exception as e:
-            print(e)
-            return None
-
-    def startApi(self):
-        self.__changeIP()
-        self.__api = subprocess.Popen(
-            'set PORT={} && node app.js'.format(self.port),
-            cwd=r"G:\163music",
-            stdout=None,
-            stderr=None,
-            shell=True
-        )
-        print('pid: {} api started port: {}'.format(self.__api.pid, self.port))
-
-    def stopApi(self):
-        self.__api.send_signal(signal.CTRL_C_EVENT)
-        # self.__api.terminate()
-
-
-api = api()
-api.startApi()
-time.sleep(5)
-api.stopApi()
+ua = UserAgent()
+headers = {
+    'UserAgent':ua.chrome
+}
+url = r'http://localhost:3000/playlist/catlist'
+json = requests.get(url,headers).json()
+categorie_data = json['categories']
+catlist = json['sub']
+print(categorie_data)
+with open('catlist.txt','w',encoding='utf-8') as f:
+    for item in categorie_data:
+        f.write(categorie_data[item] + ' ')
+    f.write('\n')
+    for i in range(len(catlist)):
+        f.write(str(i) + ' ' + catlist[i]['name'] + '\n')
