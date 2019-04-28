d0c489fbef9da7552ed494b2c0c913ebdfc25751
zooter_z <zooter_z@126.com>
2019-04-28 22:21:15 +0800

    no message

--- MongodbTest/plotlytest.py
+++ MongodbTest/plotlytest.py
@@ -1,7 +1,23 @@
-import plotly
-import plotly.graph_objs as go
+import pymongo
+import openpyxl
 
-plotly.offline.plot({
-    "data": [go.Scatter(x=[1, 2, 3, 4], y=[4, 3, 2, 1])],
-    "layout": go.Layout(title="hello world")
-}, auto_open=True)
+connstr = 'mongodb://zooter:zzc()1214@www.zooter.com.cn:27017/admin'
+client = pymongo.MongoClient(connstr)
+db = client['163music']
+cl = db['userinfos']
+
+data = []
+rows = ['UID', 'NICKNAME', 'FOLLOWS', 'FOLLOWEDS', 'LISTENSONG', 'LISTEN', 'SUBSCRIBED', 'CITY']
+wb = openpyxl.load_workbook('T_User.xlsx')
+sheet = wb['T_User']
+for line in sheet:
+    dic = {}
+    for i,v in enumerate(line):
+        dic[rows[i]] = v.value
+        print(v.value, end=' ')
+    print('\n')
+    data.append(dic)
+
+cl.insert_many(data)
+for item in cl.find():
+    print(item)
