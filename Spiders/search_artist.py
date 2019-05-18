e508c56a75b5a8211c7aad5c23d0088b5f830d9f
zooter_z <zooter_z@126.com>
2019-05-18 23:20:20 +0800

    多线程

--- Spiders/search_artist.py
+++ Spiders/search_artist.py
@@ -1,8 +1,8 @@
 import requests
 import csv
 import sys
-from fake_useragent import UserAgent
-from Helper.ApiHelper import api
+from Helper.ApiHelper import comments
+from concurrent.futures import ThreadPoolExecutor
 
 def get_artist_id(name, api):
     api.startApi()
@@ -23,50 +23,30 @@ def get_songs(id, api):
     api.stopApi()
     return songs
 
-def get_comments(song_id, api):
-    # session = requests.Session()
-    # session.keep_alive = False
-    # # session.adapters.DEFAULT_RETRIES = 5
-    # requests.adapters.DEFAULT_RETRIES = 5
-    ua = UserAgent()
-    headers = {
-        'UserAgent':ua.random
-    }
-    api.startApi()
+def request_comment(url):
+    resp = requests.get(url=url)
     comments = []
+    for item in resp.json()['comments']:
+        print(len(resp.json()['comments']))
+        comments.append(
+            {'userId': item['user']['userId'], 'nickname': item['user']['nickname'], 'likedCount': item['likedCount'],
+             'content': item['content']})
+        print(sys.getsizeof(comments))
+
+def get_comments(song_id, song_name, api):
+    api.startApi()
     url_get_comment = 'http://localhost:3000/comment/music?id={}&limit=100&offset={}'.format(song_id, 0)
-    resp = requests.get(url=url_get_comment, headers = headers)
+    resp = requests.get(url=url_get_comment)
     total = resp.json()['total']
-    for i in range(0, 5100, 100):
-        url_get_comment = 'http://localhost:3000/comment/music?id={}&limit=100&offset={}'.format(song_id, i)
-        resp = requests.get(url=url_get_comment, headers = headers)
-        for item in resp.json()['comments']:
-            comments.append({'userId':item['user']['userId'], 'nickname':item['user']['nickname'], 'likedCount':item['likedCount'], 'content':item['content']})
-        print(sys.getsizeof(comments))
-    for i in range(total - 5000, total + 100, 100):
-        url_get_comment = 'http://localhost:3000/comment/music?id={}&limit=100&offset={}'.format(song_id, i)
-        resp = requests.get(url=url_get_comment, headers=headers)
-        for item in resp.json()['comments']:
-            comments.append({'userId': item['user']['userId'], 'nickname': item['user']['nickname'],
-                             'likedCount': item['likedCount'], 'content': item['content']})
-        print(sys.getsizeof(comments))
+    urls = ['http://localhost:3000/comment/music?id={}&limit=100&offset={}'.format(song_id, i) for i in range(0, total + 100, 100)]
+    with ThreadPoolExecutor(128) as executor:
+        executor.map(request_comment, urls)
     api.stopApi()
-    with open('comments.csv', 'a', encoding='utf-8-sig', newline='') as f:
+    print(comments)
+    with open('{}-{}.csv'.format(song_id, song_name), 'a', encoding='utf-8-sig', newline='') as f:
         w = csv.writer(f, dialect='excel')
         w.writerow(['userId', 'nickname', 'likedCount', 'content'])
         for line in comments:
             w.writerow([str(line['userId']), str(line['nickname']), str(line['likedCount']), str(line['content'])])
 
-def testa(api):
-    url = 'http://localhost:3000/comment/music?id=210049&limit=100&offset='
-    ua = UserAgent()
-    api.startApi()
-    headers = {
-        'UserAgent': ua.random
-    }
-    try:
-        for i in range(0, 26000, 100):
-            resp = requests.get(url=url + str(i), headers=headers)
-            print(i, resp.status_code, resp.json()['code'])
-    finally:
-        api.stopApi()
+
--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 import sys
 from Helper.ApiHelper import api
-from Spiders.search_artist import get_artist_id, get_songs, get_comments, testa
-
+from Spiders.search_artist import get_artist_id, get_songs, get_comments
 
 Api = api()
 name = input('请输入歌手名：')
@@ -9,5 +8,5 @@ id = get_artist_id(name, Api)
 songs = get_songs(id, Api)
 print(sys.getsizeof(id))
 print(sys.getsizeof(songs))
-get_comments(songs[0]['id'], Api)
-# testa(Api)
+for item in songs:
+    get_comments(item['id'], item['name'], Api)
