84874281f323c150602e7d00b620cd7c15aeb83b
zooter_z <zooter_z@126.com>
2019-05-23 23:07:33 +0800

    mysql迁移到mongodb

--- Spiders/search_artist.py
+++ Spiders/search_artist.py
@@ -1,72 +1,62 @@
 import requests
-import csv
 import sys
-from Helper import DB
-from sqlalchemy.orm import sessionmaker
 from concurrent.futures import ThreadPoolExecutor
 
 def get_artist_id(name, api):
+    url_search_artist = 'http://localhost:3000/search?type=100&keywords={}'.format(name)
+    resp = requests.get(url = url_search_artist)
     try:
-        api.startApi()
-        url_search_artist = 'http://localhost:3000/search?type=100&keywords={}'.format(name)
-        resp = requests.get(url = url_search_artist)
         artist = resp.json()['result']['artists'][0]
         print(artist['id'], artist['name'])
-        session = sessionmaker(bind=DB.engine)
-        session = session()
-        DB.Base.metadata.create_all(DB.engine)
-        row = DB.Atists(Aid = artist['id'], Aname = artist['name'])
-        session.add(row)
-        session.commit()
-        session.close()
-    finally:
+    except Exception as e:
         api.stopApi()
-    return artist['id']
-
-def get_songs(id, api):
-    try:
         api.startApi()
-        url_get_artist_songs = 'http://localhost:3000/artists?id={}'.format(id)
-        resp = requests.get(url = url_get_artist_songs)
-        session = sessionmaker(bind=DB.engine)
-        session = session()
-        DB.Base.metadata.create_all(DB.engine)
-        songs = []
-        for item in resp.json()['hotSongs']:
-            songs.append({'id':item['id'], 'name':item['name']})
-            row = DB.Songs(Sid=item['id'], Sname=item['name'], artist_Aid=id)
-            session.add(row)
-        session.commit()
-        session.close()
-    finally:
+        return get_artist_id(name, api)
+    return (artist['id'], artist['name'])
+
+def get_songs(artist_info, api):
+    url_get_artist_songs = 'http://localhost:3000/artists?id={}'.format(artist_info[0])
+    resp = requests.get(url = url_get_artist_songs)
+    songs = []
+    try:
+        for index, item in enumerate(resp.json()['hotSongs']):
+            songs.append({'rating':index + 1, 'aid':artist_info[0], 'aname':artist_info[1], 'sid':item['id'], 'sname':item['name']})
+    except Exception as e:
+        print('get_songs' + e)
         api.stopApi()
+        api.startApi()
+        return get_songs(artist_info, api)
     return songs
 
-def request_comment(url, songid):
+def request_comment(url, song_info, db, api):
+    collection = db[str(song_info['rating']) + '@' +str(song_info['sid']) + '@' + str(song_info['sname'])]
     resp = requests.get(url=url)
-    session = sessionmaker(bind=DB.engine)
-    session = session()
-    DB.Base.metadata.create_all(DB.engine)
-    for item in resp.json()['comments']:
-        print(len(resp.json()['comments']))
-        comments.append(
-            {'commentId':item['commentId'], 'userId': item['user']['userId'], 'nickname': item['user']['nickname'], 'likedCount': item['likedCount'],
-             'content': item['content']})
-        row = DB.Comments(Cid=item['commentId'], Uid=item['user']['userId'], Uname=item['user']['nickname'], LikeCount=item['likedCount'], Content=item['content'], song_Sid=songid)
-        session.add(row)
-        print(sys.getsizeof(comments))
-    session.commit()
-    session.close()
+    comments = []
+    try:
+        for item in resp.json()['comments']:
+            comments.append(
+                {'aid':song_info['aid'],'aname':song_info['aname'],'sid':song_info['sid'],'sname':song_info['sname'],'commentId':item['commentId'], 'userId': item['user']['userId'], 'nickname': item['user']['nickname'], 'likedCount': item['likedCount'],
+                 'content': item['content']})
+        collection.insert_many(comments)
+    except Exception as e:
+        print('request_comment' + e)
+        api.stopApi()
+        api.startApi()
+        request_comment(url, song_info, db, api)
 
-def get_comments(song_id, song_name, api):
-    api.startApi()
-    url_get_comment = 'http://localhost:3000/comment/music?id={}&limit=100&offset={}'.format(song_id, 0)
-    resp = requests.get(url=url_get_comment)
-    total = resp.json()['total']
-    urls = ['http://localhost:3000/comment/music?id={}&limit=100&offset={}'.format(song_id, i) for i in range(0, total + 100, 100)]
-    ids = [song_id for i in range(0, total + 100, 100)]
-    with ThreadPoolExecutor(128) as executor:
-        executor.map(request_comment, urls, ids)
-    api.stopApi()
-
-
+def get_comments_multi_thread(song_info, api, db):
+    url_get_comment = 'http://localhost:3000/comment/music?id={}&limit=100&offset={}'.format(song_info['sid'], 0)
+    try:
+        resp = requests.get(url=url_get_comment)
+        total = resp.json()['total']
+    except Exception as e:
+        api.stopApi()
+        api.startApi()
+        resp = requests.get(url=url_get_comment)
+        total = resp.json()['total']
+    urls = ['http://localhost:3000/comment/music?id={}&limit=100&offset={}'.format(song_info['sid'], i) for i in range(0, total + 100, 100)]
+    song_infos = [song_info for i in range(0, total + 100, 100)]
+    dbs = [db for i in range(0, total + 100, 100)]
+    apis = [api for i in range(0, total + 100, 100)]
+    with ThreadPoolExecutor(64) as executor:
+        executor.map(request_comment, urls, song_infos, dbs, apis)
--- run.py
+++ run.py
@@ -1,12 +1,22 @@
 import sys
 from Helper.ApiHelper import api
-from Spiders.search_artist import get_artist_id, get_songs, get_comments
+from Helper.SqlHelper import getMongoTx
+from Spiders.search_artist import get_artist_id, get_songs, get_comments_multi_thread
+import pymongo
+import time
 
+start_time = time.time()
+client = pymongo.MongoClient(getMongoTx())
+db = client['163music']
 Api = api()
+Api.startApi()
 name = input('请输入歌手名：').strip()
-id = get_artist_id(name, Api)
-songs = get_songs(id, Api)
-print(sys.getsizeof(id))
-print(sys.getsizeof(songs))
-for item in songs:
-    get_comments(item['id'], item['name'], Api)
+try:
+    artist_info = get_artist_id(name, Api)
+    songs = get_songs(artist_info, Api)
+    print(sys.getsizeof(songs))
+    for index, song_info in enumerate(songs):
+        get_comments_multi_thread(song_info, Api, db)
+finally:
+    Api.stopApi()
+print(time.time() - start_time)
