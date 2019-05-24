4f1c8402876cc50a0ad87a64621c3c9771a897ab
me <zooter_z@126.com>
2019-05-24 20:32:56 +0800

    jieba test

--- test.py
+++ test.py
@@ -1,17 +1,27 @@
 from Helper.SqlHelper import getMongoTx
 import pymongo
-import pkuseg
+import jieba
+import collections
 
-
-# client = pymongo.MongoClient(getMongoTx())
-# db = client['163music']
-# collection_names = db.list_collection_names()
-# collection = db.get_collection(collection_names[0])
-# result = collection.find({},{'_id':0, 'content':1})
-# comments = []
-# with open('comments.txt', 'w', encoding='utf-8') as f:
-#     for item in result:
-#         comments.append(item['content'])
-#         f.write(item['content'])
+def save_comments():
+    client = pymongo.MongoClient(getMongoTx())
+    db = client['163music']
+    collection_names = db.list_collection_names()
+    collection = db.get_collection(collection_names[0])
+    result = collection.find({}, {'_id': 0, 'content': 1})
+    with open('comments.txt', 'w', encoding='utf-8') as f:
+        for item in result:
+            f.write(item['content'].replace('\n', '') + '\n')
+def partition():
+    result = []
+    with open('comments.txt', 'r', encoding='utf-8') as s:
+        comments = s.readlines()
+        for item in comments:
+            words = jieba.cut(item)
+            for word in words:
+                result.append(word)
+    with open('result.txt', 'a+', encoding='utf-8') as r:
+        for word in result:
+            r.write(word + '\n')
 if __name__ == '__main__':
-    pkuseg.test('test.txt', 'output.txt', nthread=8)
+    partition()
