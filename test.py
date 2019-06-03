0e94abd276ef4f1773e24d2bc23ca35fa27d098f
me <zooter_z@126.com>
2019-06-03 18:33:18 +0800

    test

--- test.py
+++ test.py
@@ -1,60 +1,29 @@
-from Helper.SqlHelper import getMongoTx
-from PIL import Image
-import pymongo
-import jieba
-import collections
-import re
-import wordcloud
-import numpy
+import requests
+from bs4 import BeautifulSoup
 
 
-def save_comments():
-    client = pymongo.MongoClient(getMongoTx())
-    db = client['163music']
-    collection_names = db.list_collection_names()
-    collection = db.get_collection(collection_names[0])
-    result = collection.find({}, {'_id': 0, 'content': 1})
-    with open('comments.txt', 'w', encoding='utf-8') as f:
-        for item in result:
-            f.write(item['content'].replace('\n', '') + '\n')
-def partition():
-    result = []
-    pattern = re.compile(u'\t|\n|\.|-|:|;|\)|\(|\?|"|。|，|？|！|“|”|\[|\]|—|《|》| |~|]|（|）|…|、|\+|：')
-    with open('comments.txt', 'r', encoding='utf-8') as s:
-        comments = s.readlines()
-        for item in comments:
-            words = jieba.cut(item)
-            for word in words:
-                if not re.match(pattern, word) and len(word) != 1:
-                    result.append(word)
-            result.pop()
-    return result
+def get_loccodes():
+    # 中华人民共和国行政区划代码
+    url = 'http://www.mca.gov.cn/article/sj/xzqh/2019/201901-06/201905271445.html'
+    locs = {}
+    try:
+        r = requests.get(url)
+        if r.status_code == 200:
+            r.encoding = "utf-8"
+            soup = BeautifulSoup(r.text, 'lxml')
+            items = soup.find_all('tr', attrs={"height": "19"})
+            # 城市编码的个数
+            print(print(len(items)))
+            for item in items:
+                # 提取数据
+                # print(item.find_all('td')[1].text, item.find_all('td')[2].text)
+                locs[item.find_all('td')[1].text] = item.find_all('td')[2].text
+    except Exception as e:
+        print("获取失败！" + str(e))
 
-def save_to_file(result):
-    with open('result.txt', 'a+', encoding='utf-8', newline='') as r:
-        for word in result:
-            r.write(word + '\n')
-
-def word_count(word_list):
-    word_counts = collections.Counter(word_list)
-    word_counts_top100 = word_counts.most_common(100)
-    return word_counts
-    # with open('top100.txt', 'w', encoding='utf-8') as f:
-    #     for i in word_counts_top100:
-    #         f.write(str(i) + '\n')
-
-def generate_image(word_counts):
-    mask = numpy.array(Image.open('picture.png'))
-    w = wordcloud.WordCloud(
-        width=960,
-        height=540,
-        font_path='Deng.ttf',
-        mask=mask,
-        max_words=100,
-        max_font_size = 100
-    )
-    w.generate_from_frequencies(word_counts)
-    w.to_file('test.png')
+    # 可视化输出
+    # print(locs)
+    return locs
 
 if __name__ == '__main__':
-    generate_image(word_count(partition()))
+    print(get_loccodes())
--- word segmentation test/test.py
+++ word segmentation test/test.py
@@ -1,5 +1,61 @@
-import pkuseg
+from Helper.SqlHelper import getMongoTx
+from PIL import Image
+import pymongo
+import jieba
+import collections
+import re
+import wordcloud
+import numpy
 
-seg = pkuseg.pkuseg()
-text = seg.cut('Redmi K20 已经可以定金预售了，骁龙 855、索尼 4800 万、4000mAh 电池、升降式前置、屏幕指纹识别……等个售价吧（隐约有种 K20 会比米酒卖的还好的预感')
-print(text)
+
+def save_comments():
+    client = pymongo.MongoClient(getMongoTx())
+    db = client['163music']
+    collection_names = db.list_collection_names()
+    collection = db.get_collection(collection_names[0])
+    result = collection.find({}, {'_id': 0, 'content': 1})
+    with open('comments.txt', 'w', encoding='utf-8') as f:
+        for item in result:
+            f.write(item['content'].replace('\n', '') + '\n')
+def partition():
+    result = []
+    pattern = re.compile(u'\t|\n|\.|-|:|;|\)|\(|\?|"|。|，|？|！|“|”|\[|\]|—|《|》| |~|]|（|）|…|、|\+|：')
+    with open('comments.txt', 'r', encoding='utf-8') as s:
+        comments = s.readlines()
+        for item in comments:
+            words = jieba.cut(item)
+            for word in words:
+                if not re.match(pattern, word) and len(word) != 1:
+                    result.append(word)
+            result.pop()
+    return result
+
+def save_to_file(result):
+    '''保存partition的结果到文件'''
+    with open('result.txt', 'a+', encoding='utf-8', newline='') as r:
+        for word in result:
+            r.write(word + '\n')
+
+def word_count(word_list):
+    word_counts = collections.Counter(word_list)
+    word_counts_top100 = word_counts.most_common(100)
+    return word_counts
+    # with open('top100.txt', 'w', encoding='utf-8') as f:
+    #     for i in word_counts_top100:
+    #         f.write(str(i) + '\n')
+
+def generate_image(word_counts):
+    mask = numpy.array(Image.open('picture.png'))
+    w = wordcloud.WordCloud(
+        width=960,
+        height=540,
+        font_path='Deng.ttf',
+        mask=mask,
+        max_words=100,
+        max_font_size = 100
+    )
+    w.generate_from_frequencies(word_counts)
+    w.to_file('test.png')
+
+if __name__ == '__main__':
+    generate_image(word_count(partition()))
